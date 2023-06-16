d0d340d518b16fb3aa22d59ad71d2dea1eababe6
GurenLagann <dragonwits@gmail.com>
2023-06-15 22:00:07 -0300

    modificações de pastas

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import { estoqueRoutes } from "./routes/estoque";
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
+app.register(estoqueRoutes)
 
-  return users
-})
 
 app.listen({
   port: 3333,
