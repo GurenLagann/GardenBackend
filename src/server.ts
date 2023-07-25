55776fcfac492ebdc4ebaf6a321d0bfc1dbf193e
GurenLagann <dragonwits@gmail.com>
2023-07-25 01:48:37 -0300

    configurações do CORS adicionadas

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import { router } from "./router";
+import cors from "cors";
 
 const app = express()
 
 app.use(express.json())
 app.use(router)
+
+app.use(cors({
+  origin: true,
+  credentials: true
+}))
+
+
 
 const PORT = process.env.PORT || 3000;
 
